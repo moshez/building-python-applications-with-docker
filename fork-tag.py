c01e08c69de80fb148e49c918559a4aec2fa85f1
Moshe Zadka <zadka.moshe@gmail.com>
2017-10-30 09:38:46 -0700

    frop

--- fork-tag.py
+++ fork-tag.py
@@ -4,10 +4,11 @@ import subprocess
 def cmd(*args):
     subprocess.check_call(list(args))
 
+IMAGE = 'pypy:3-slim'
 TAG = (datetime.datetime.now()
                .isoformat().replace(':', '-')
                            .replace('.', '-'))
 NAME = 'moshez/pypy3:' + TAG
-cmd('docker', 'pull', 'pypy:3-slim')
-cmd('docker', 'tag', 'pypy:3-slim', NAME)
+cmd('docker', 'pull', IMAGE)
+cmd('docker', 'tag', IMAGE, NAME)
 cmd('docker', 'push', NAME)
